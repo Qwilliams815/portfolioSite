bf3bcfab64d49f7cac4771cdf429805226c3bf96
Brian Williams <brianman815@gmail.com>
2021-10-20 17:54:55 -0700

    A file was deleted

---
+++ script.js
@@ -0,0 +1,14 @@
+const contactModalX = document.querySelector(".modal-x");
+const contactModal = document.querySelector(".contact-modal")
+const contactButton = document.querySelector(".contact-button")
+const blur = document.querySelector(".blur-wrapper")
+
+contactModalX.addEventListener("click", (e) => {
+    contactModal.classList.toggle("hide-window");
+    blur.classList.toggle("blur")
+});
+
+contactButton.addEventListener("click", (e) => {
+    contactModal.classList.toggle("hide-window");
+    blur.classList.toggle("blur")
+});
