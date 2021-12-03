3d3ee6712414f7f9b0d7774a83b2527135c06d5f
Brian Williams <brianman815@gmail.com>
2021-12-02 17:46:56 -0800

    Updated bio, Added bio collapse for mobile, Added course showcase

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const contactModalX = document.querySelector(".modal-x");
 const contactModal = document.querySelector(".contact-modal");
 const contactButton = document.querySelector(".contact-button");
+
 const leftButton = document.querySelector(".left-arrow");
 const rightButton = document.querySelector(".right-arrow");
 const skillsCarousel = document.querySelector('.skills-carousel');
-const slideContainer = document.querySelector(".slide-container")
+const slideContainer = document.querySelector(".slide-container");
+
+const readMoreButton = document.querySelector(".read-more-btn");
+const readMoreButtonText = document.querySelector(".read-more-btn-text");
+const moreAboutText = document.querySelector(".more-text");
+const aboutTextStyles = window.getComputedStyle(moreAboutText);
 
 const blur = document.querySelector(".blur-wrapper")
 
@@ -24,4 +30,14 @@ leftButton.addEventListener("click", (e) => {
 
 rightButton.addEventListener("click", (e) => {
     slideContainer.style.transform = 'translateX(-' + (slideContainer.clientWidth - skillsCarousel.clientWidth) + 'px)';
+})
+
+readMoreButton.addEventListener("click", (e) => {
+    moreAboutText.classList.toggle("toggle-more-text")
+    
+    if (aboutTextStyles.display === 'none') {
+        readMoreButtonText.innerHTML = 'Read More';
+    } else {
+        readMoreButtonText.innerHTML = 'Read Less';
+    }
 })
